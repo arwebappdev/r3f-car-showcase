715d921e058c0edb50038c27c0400a9fca91ca36
ar-mansoori <ar.appsoftwares@gmail.com>
2025-08-06 23:54:56 +0530

    Started sec1

--- src/Sections.jsx
+++ src/Sections.jsx
@@ -5,9 +5,15 @@ export default function Sections() {
     <div className="">
       <section
         id="section-1"
-        className="h-screen flex items-center justify-center text-white ring-2"
+        className="h-screen pt-[5rem] justify-center text-white border border-white grid grid-cols-1"
       >
-        <h1 className="text-4xl">Section 1</h1>
+        <div className="relative text-[50px] font-sans font-semibold p-5 border-2 border-blue-50 h-fit w-fit">
+          CLASS PIONEER AND ETERNAL BENCHMARK.
+        </div>
+        <div className="relative place-self-end justify-center mb-[2rem] px-[3rem] text-gray-400 text-center text-[25px] border-2 border-blue-50">
+          The BMW M3 E30. Compact, powerful, sporty and extremely successful on
+          the racetrack: the incredible story of the first BMW M3.
+        </div>
       </section>
       <section
         id="section-2"
